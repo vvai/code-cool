e4ded4341752c7064e5b497f4764aed898bae134
Roman Liutikov <roman01la@romanliutikov.com>
2017-11-20 13:16:42 +0200

    rm unnecessary stuff

--- src/components/screenshotPreview/index.js
+++ src/components/screenshotPreview/index.js
@@ -1,28 +1,31 @@
-import { h, Component } from 'preact';
-import hljs from 'highlight.js';
+import { h, Component } from "preact";
+import hljs from "highlight.js";
 
 export default class ScreenshotPreview extends Component {
+  componentDidMount() {
+    hljs.highlightBlock(this.code);
+  }
 
-	componentDidMount() {
-		hljs.highlightBlock(this.code);
-	}
+  componentDidUpdate() {
+    this.code.innerHTML = this.props.value;
+    hljs.highlightBlock(this.code);
+  }
 
-	componentDidUpdate() {
-		this.code.innerHTML = this.props.value;
-		hljs.highlightBlock(this.code);
-	}
-
-	render() {
-		const backgroundStyle = `background-color: ${this.props.background}`;
-		const fontStyle = `font-size: ${this.props.fontSize}px`;
-		return (<pre class="viewport" style={backgroundStyle}>
-			<code
-				ref={(code) => { this.code = code; }}
-				class="javascript display-code"
-				style={fontStyle}
-			>
-				{this.props.value}
-			</code>
-		</pre>);
-	}
+  render() {
+    const backgroundStyle = `background-color: ${this.props.background}`;
+    const fontStyle = `font-size: ${this.props.fontSize}px`;
+    return (
+      <pre class="viewport" style={backgroundStyle}>
+        <code
+          ref={code => {
+            this.code = code;
+          }}
+          class="clojure display-code"
+          style={fontStyle}
+        >
+          {this.props.value}
+        </code>
+      </pre>
+    );
+  }
 }
